6bfb52f64e9a1b41baca8799a58fd4fb1fb44e92
ss <sqndxr@gmail.com>
2026-02-03 07:38:30 +0100

    init

---
+++ pkg/landing/src/lib/components/custom/logo.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { theme } from "mode-watcher";
+
+    import barekey_light from "$lib/assets/barekey-light.png";
+    import barekey_dark from "$lib/assets/barekey-dark.png";
+
+</script>
+{#if theme.current == "light"}
+    <img src={barekey_dark} alt="Barekey" class="h-10">
+{:else}
+    <img src={barekey_light} alt="Barekey" class="h-10">
+{/if}
---
+++ pkg/landing/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import './layout.css';
+	import "@fontsource/google-sans-flex";
+
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link href="https://fonts.cdnfonts.com/css/pp-neue-montreal" rel="stylesheet">
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher />
+{@render children()}
